3678a11c62cbd973e695eeddb6d68870a2379486
SeptRepo <adilsid756@outlook.com>
2021-09-17 16:46:39 +0530

    My First Commit

---
+++ sampleapp/src/main/java/org/capg/demo/BootClass.java
@@ -0,0 +1,11 @@
+package org.capg.demo;
+
+public class BootClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello from STS!");
+
+	}
+
+}
