3bb0f2bb2f0a70abcdbae34298f78be00c486d38
SeptRepo <adilsid756@outlook.com>
2021-09-20 10:49:10 +0530

    Show method added!!!

--- sampleapp/src/main/java/org/capg/demo/BootClass.java
+++ sampleapp/src/main/java/org/capg/demo/BootClass.java
@@ -5,6 +5,15 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello from STS!");
+show();
+	}
+	public static void show(){
+		int[] arr= {1,2,3,4,5,6};
+		for(int n:arr)
+		{
+			sum+=n;
+			System.out.println("Show methoad here!");
+		}
 
 	}
 
